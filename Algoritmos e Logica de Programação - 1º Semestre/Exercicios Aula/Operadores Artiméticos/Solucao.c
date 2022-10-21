cfb605c4fb5ee81365a651ef7a9956ba211b46ba
Leonardo Sextare <leonardosextare@gmail.com>
2022-10-21 06:58:18 -0300

    Operadores Aritmeticos

---
+++ Algoritmos e Logica de Programação - 1º Semestre/Exercicios Aula/Operadores Artiméticos/Solucao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main(){
+    setlocale(LC_ALL,"");
+
+    float valorProduto, quantidade, parcela;
+
+    printf("");
+    scanf("");
+
+}
